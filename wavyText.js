4765d7dd0ea6a213432b11687f3c1e62831e1bd6
ocilical <32717754+ocilical@users.noreply.github.com>
2020-10-24 12:13:56 -0400

    wavy text overhaul!
    the wavy text now works inline, and applies to all elements of class wavy! amplitude can also be specified with the ampl attribute

--- wavyText.js
+++ wavyText.js
@@ -1,41 +1,49 @@
-function initWavyText(id) {
-    let div = document.getElementById(id)
-    let text = div.firstElementChild.innerText;
-    div.removeChild(div.firstElementChild);
+function initWavyText() {
+    for (let j = 0; j < document.getElementsByClassName("wavy").length; j++) {
+        let span = document.getElementsByClassName("wavy")[j];
+        let text = span.innerHTML;
+        span.innerHTML = "";
 
-    for (let i = 0; i < text.length; i++) {
-        let el = document.createElement('span');
-        let textEl = document.createElement("p")
-        textEl.appendChild(document.createTextNode(text.charAt(i)))
-        el.setAttribute("phase", i);
-        el.appendChild(textEl);
-        div.appendChild(el)
+        for (let i = 0; i < text.length; i++) {
+            let el = document.createElement('span');
+            let textEl = document.createElement("p")
+            textEl.appendChild(document.createTextNode(text.charAt(i)))
+            el.setAttribute("phase", i);
+            el.appendChild(textEl);
+            span.appendChild(el)
+        }
     }
 }
 
-initWavyText("wavy")
+initWavyText();
 
 let time = 0;
 let period = 2;
-let amplitude = 5;
+
 let wavyText = document.getElementById("wavy");
 
 function anim() {
-    if(period * time > 2 * Math.PI) {
+    if (period * time > 2 * Math.PI) {
         time = 0;
     }
-    for (let i = 0; i < wavyText.children.length; i++) {
-        let el = wavyText.children[i];
-        let phase = el.getAttribute("phase");
-        
-        let offsety = amplitude * Math.sin(period * time + phase/2)
-        let offsetx = -amplitude * Math.cos(period * time + phase/2)
-        
-        el.style.transform = `translate(${offsetx}px, ${offsety}px)`
+    for (let j = 0; j < document.getElementsByClassName("wavy").length; j++) {
+
+        let wavyText = document.getElementsByClassName("wavy")[j];
+        let amplitude = wavyText.getAttribute("ampl") || 2; //2 is default
+        for (let i = 0; i < wavyText.children.length; i++) {
+            let el = wavyText.children[i];
+            let phase = el.getAttribute("phase");
+
+            let offsety = amplitude * Math.sin(period * time + phase / 2)
+            let offsetx = -amplitude * Math.cos(period * time + phase / 2)
+
+            el.style.transform = `translate(${offsetx}px, ${offsety}px)`
+        }
+
     }
     time += 6 / 100;
     window.requestAnimationFrame(anim);
-    
+
 }
 
 window.requestAnimationFrame(anim);
